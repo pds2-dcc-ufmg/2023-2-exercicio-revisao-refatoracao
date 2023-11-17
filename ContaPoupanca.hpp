77a2cc26bdaab8dfa49c595c3d091cab10ccf6b8
wilsong555 <143765603+wilsong555@users.noreply.github.com>
2023-11-17 15:55:15 -0300

    Update ContaPoupanca.hpp

--- ContaPoupanca.hpp
+++ ContaPoupanca.hpp
@@ -5,14 +5,10 @@
 #include "ContaBancaria.hpp"
 
 class conta_poupanca : public ContaBancaria {
-    public:
-        void calcularJuros() {
-            double juros = saldo * taxaJuros / 100;
-            saldo += juros;
-            std::cout << "Juros de R$" << juros << " calculados e adicionados à conta.\n";
-        }
-        double taxaJuros;
+private:
+    double taxaJuros;
+public:
+    ContaPoupanca(std::string titular, double saldo, double taxaJuros);
+    void calcularJuros();
 };
-
-
 #endif
