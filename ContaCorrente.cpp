b0ff39f1f39e92b52a3f46b8057cf7c0651c3ab0
wilsong555 <143765603+wilsong555@users.noreply.github.com>
2023-11-17 15:34:49 -0300

    Create ContaCorrente.cpp

---
+++ ContaCorrente.cpp
@@ -0,0 +1,16 @@
+#include "ContaCorrente.hpp"
+
+ContaCorrente::ContaCorrente(std::string titular, double saldo, double limiteChequeEspecial): 
+ContaBancaria(titular, saldo), limiteChequeEspecial(limiteChequeEspecial) {}
+
+void ContaCorrente::usarChequeEspecial(double valor) {
+
+  double valorTotal = saldo + limiteChequeEspecial;
+
+      if (valor > 0 && valor <= valorTotal) {
+            saldo -= valor;
+            std::cout << "Uso de cheque especial de R$" << valor << " efetuado com sucesso.\n";
+      } else {
+           std::cout << "Uso de cheque especial inválido. Verifique o valor ou limite.\n";
+      }
+}
