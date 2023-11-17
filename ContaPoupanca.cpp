2974f310bfc36624b52a8236ed4fd2cb22d3ab45
adlerfaria <adlerfprofissional@gmail.com>
2023-11-16 21:37:20 -0300

    codigo arrumado

---
+++ ContaPoupanca.cpp
@@ -0,0 +1,26 @@
+#include "ContaPoupanca.hpp"
+#include <iostream>
+
+ContaPoupanca::ContaPoupanca(const std::string &titular, double saldo, double taxa_juros)
+    : ContaBancaria(titular, saldo), taxa_juros(taxa_juros) {}
+
+void ContaPoupanca::calcular_juros() {
+    if (taxa_juros >= 0) {
+        double juros = saldo * taxa_juros / 100;
+        adicionar_juros_ao_saldo(juros);
+    }
+}
+
+void ContaPoupanca::adicionar_juros_ao_saldo(double juros) {
+    set_saldo(juros + get_saldo());
+    std::cout << "Juros de R$" << juros << " calculados e adicionados à conta.\n";
+}
+
+double ContaPoupanca::get_juros() const {
+    return taxa_juros;
+}
+
+void ContaPoupanca::set_juros(double _taxa_juros) {
+    taxa_juros = _taxa_juros;
+}
+
