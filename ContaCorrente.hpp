0256f182f7a5cedc61cfb93ecefc51197c9b5d63
wilsong555 <143765603+wilsong555@users.noreply.github.com>
2023-11-17 15:25:06 -0300

    Update ContaCorrente.hpp

--- ContaCorrente.hpp
+++ ContaCorrente.hpp
@@ -5,19 +5,10 @@
 #include "ContaBancaria.hpp"
 
 class conta_corrente : public ContaBancaria {
-public:
-void usarChequeEspecial(double valor) {
-        double valorTotal = saldo + limiteChequeEspecial;
-        if (valor > 0 && valor <= valorTotal) {
-            saldo -= valor;
-            std::cout << "Uso de cheque especial de R$" << valor << " efetuado com sucesso.\n";
-        } else {
-            std::cout << "Uso de cheque especial inválido. Verifique o valor ou limite.\n";
-        }
-    }
-
+private:
     double limiteChequeEspecial;
+public:
+    ContaCorrente(std::string titular, double saldo, double limiteChequeEspecial);
+    void usarChequeEspecial(double valor);
 };
-
-
 #endif
