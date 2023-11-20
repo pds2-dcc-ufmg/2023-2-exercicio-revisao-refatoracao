67060565a0fb54bda116ee7781d484cb64ba075e
Gustavoymf <gustavoyoshiomf>
2023-11-20 17:54:38 -0300

    Revisão de código e Rafatoração

--- Banco.hpp
+++ Banco.hpp
@@ -9,18 +9,11 @@
 using namespace std;
 
 class Banco {
-public:
-    void adicionarConta(ContaBancaria* conta) {
-        contas.push_back(conta);
-    }
-
-    void exibir_todas_contas() {
-        for (ContaBancaria* conta : contas) {
-            std::cout << "Saldo da conta de " << conta->titular << ": R$" << conta->saldo << "\n";
-        }
-    }
-
+private:
     std::vector<ContaBancaria*> contas;
+public:
+    void adicionarConta(ContaBancaria* conta);
+    void exibir_todas_contas();
 };
 
 #endif
