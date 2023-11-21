fae6d564f517c9a8400f918a0bccc067bae95cb1
wilsong555 <143765603+wilsong555@users.noreply.github.com>
2023-11-21 20:38:50 -0300

    Update Banco.hpp

--- Banco.hpp
+++ Banco.hpp
@@ -1,21 +1,17 @@
 #ifndef BANCO_H
 #define BANCO_H
 
-#include <iostream>
-#include <string>
-#include <vector>
-#include "ContaBancaria.hpp"
+#include "ContaPoupanca.hpp"
 #include "ContaCorrente.hpp"
-#include "ContaPoupança.hpp"
 
 class Banco {
 private:
 std::vector<ContaBancaria*> contas;
 public:
-ContaCorrente* adicionarContaCorrente(std::string titular, double saldo, double limiteChequeEspecial);
-ContaPoupanca* adicionarContaPoupanca(std::string titular, double saldo, double taxaJuros);
+ContaPoupanca* adicionar_conta_poupanca(std::string titular, double saldo, double taxa_juros);
+ContaCorrente* adicionar_conta_corrente(std::string titular, double saldo, double limite_cheque_especial);
 void exibir_todas_contas();
-~Banco()
+~Banco();
 };
 
 #endif
