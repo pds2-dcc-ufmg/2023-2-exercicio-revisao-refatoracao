b1ff39157eaf74c551d3337fbf33851db77cd669
wilsong555 <143765603+wilsong555@users.noreply.github.com>
2023-11-21 20:40:00 -0300

    Update ContaBancaria.cpp

--- ContaBancaria.cpp
+++ ContaBancaria.cpp
@@ -1,25 +1,36 @@
 #include "ContaBancaria.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
 
-ContaBancaria::ContaBancaria(std::string titular, double saldo): titular(titular), saldo(saldo) {}
+ContaBancaria::ContaBancaria(std::string titular, double saldo) : titular(titular), saldo(saldo) {}
 
 void ContaBancaria::depositar(double valor) {
-
-      if (valor > 0) {
-            saldo += valor;
-            std::cout << "Depósito de R$" << valor << " efetuado com sucesso.\n";
-      } else {
-           std::cout << "Valor de depósito inválido.\n";
-      }
+    if (valor > 0) {
+        saldo += valor;
+        std::cout << "Depósito de R$" << valor << " efetuado com sucesso.\n";
+    }else {
+        std::cout << "Valor de depósito inválido.\n";
+    }
 }
+
 void ContaBancaria::sacar(double valor) {
-
-      if (valor > 0 && valor <= saldo) {
-            saldo -= valor;
-            std::cout << "Saque de R$" << valor << " efetuado com sucesso.\n";
-      } else {
-            std::cout << "Saque inválido. Verifique o valor ou saldo insuficiente.\n";
-      }
+    if (valor > 0 && valor <= saldo) {
+        saldo -= valor;
+        std::cout << "Saque de R$" << valor << " efetuado com sucesso.\n";
+    }else {
+        std::cout << "Saque inválido. Verifique o valor ou saldo insuficiente.\n";
+    }
 }
 
-std::string ContaBancaria::getTitular() { 
-  return titular; }
+double ContaBancaria::get_saldo() const {
+    return saldo;
+}
+
+std::string ContaBancaria::get_titular() const {
+    return titular;
+}
+
+void ContaBancaria::set_saldo(double _saldo){
+    saldo = _saldo;
+}
