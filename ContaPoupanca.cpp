2954a1420d24bcb264841541f7abfd0ae2830e09
wilsong555 <143765603+wilsong555@users.noreply.github.com>
2023-11-21 20:44:37 -0300

    Update ContaPoupanca.cpp

--- ContaPoupanca.cpp
+++ ContaPoupanca.cpp
@@ -1,10 +1,25 @@
 #include "ContaPoupanca.hpp"
+#include <iostream>
 
-ContaPoupanca::ContaPoupanca(std::string titular, double saldo, double taxaJuros): 
-ContaBancaria(titular, saldo), taxaJuros(taxaJuros) {}
+ContaPoupanca::ContaPoupanca(const std::string titular, double saldo, double taxa_juros)
+    : ContaBancaria(titular, saldo), taxa_juros(taxa_juros) {}
 
-void ContaPoupanca::calcularJuros() {
-      double juros = saldo * taxaJuros / 100;
-      saldo += juros;
-      std::cout << "Juros de R$" << juros << " calculados e adicionados à conta.\n";
+void ContaPoupanca::calcular_juros() {
+    if (taxa_juros >= 0) {
+        double juros = get_saldo() * taxa_juros / 100;
+        adicionar_juros_ao_saldo(juros);
+    }
+}
+
+void ContaPoupanca::adicionar_juros_ao_saldo(double juros) {
+    set_saldo(juros + get_saldo());
+    std::cout << "Juros de R$" << juros << " calculados e adicionados à conta.\n";
+}
+
+double ContaPoupanca::get_juros() const {
+    return taxa_juros;
+}
+
+void ContaPoupanca::set_juros(double _taxa_juros) {
+    taxa_juros = _taxa_juros;
 }
