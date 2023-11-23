3c063fdf0f064aa7265b10863891de1b9d63a94c
heitor <heitorleite.dev@gmail.com>
2023-11-23 19:28:02 -0300

    refatoração inicial

---
+++ Banco.cpp
@@ -0,0 +1,18 @@
+#include "Banco.hpp"
+#include "ContaBancaria.hpp"
+
+void Banco::adicionarConta(ContaBancaria* conta) {
+    contas.push_back(conta);
+}
+
+void Banco::exibir_todas_contas() {
+    for (ContaBancaria* conta : contas) {
+        std::cout << "Saldo da conta de " << conta->getTitular() << ": R$" << conta->getSaldo() << "\n";
+    }
+}
+
+Banco::~Banco() {
+    for (ContaBancaria* conta : contas) {
+        delete conta;
+    }
+}
